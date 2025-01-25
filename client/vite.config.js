a60274d3cc2a8689938db616c4a867fcb871c96b
Markiskino35 <markiskino35@Alief-Mukhlis.local>
2025-01-26 01:22:25 +0800

    chore: install new tailwind v4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -14,5 +15,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
